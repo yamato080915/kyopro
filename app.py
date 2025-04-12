4e6a449d740d20c73ef39dbb42abc2e66be65824
yamato080915 <fotonashitai18@gmail.com>
2025-04-12 23:51:33 +0900

    fixed rating graph

--- app.py
+++ app.py
@@ -13,10 +13,22 @@ def home():
 
 @app.route(f"{debug}atcoder_rating")
 def graph():
-    data = requests.get("https://atcoder.jp/users/yamato0915/history/json")
-    data = data.json()
-    data = [x for x in data if x["IsRated"]]
-    return jsonify(data)
+	data = requests.get("https://atcoder.jp/users/yamato0915/history/json")
+	data = data.json()
+	data = [x for x in data if x["IsRated"]]
+	for i in data:
+		i["Place"] = str(i["Place"])
+		dt = datetime.fromisoformat(i["EndTime"])
+		i["EndTime"] = f"{dt.year}/{dt.month}/{dt.day}"
+		if i["NewRating"]<400:i["Rank"]="gray"
+		elif i["NewRating"]<800:i["Rank"]="brown"
+		elif i["NewRating"]<1200:i["Rank"]="green"
+		elif i["NewRating"]<1600:i["Rank"]="cyan"
+		elif i["NewRating"]<2000:i["Rank"]="blue"
+		elif i["NewRating"]<2400:i["Rank"]="yellow"
+		elif i["NewRating"]<2800:i["Rank"]="orange"
+		else:i["Rank"]="red"
+	return jsonify(data)
 
 @app.route(f"{debug}contests")
 def contests():
